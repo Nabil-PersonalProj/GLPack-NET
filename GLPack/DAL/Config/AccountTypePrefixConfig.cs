ba0ca551ab4ddcfeeb6350dacfda2257081e3b03
Nabil <Nabil@DESKTOP-G0QI2LU>
2026-03-31 14:30:43 +0800

    AccountType Table

---
+++ GLPack/DAL/Config/AccountTypePrefixConfig.cs
@@ -0,0 +1,24 @@
+﻿using GLPack.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GLPack.DAL.Config
+{
+    public class AccountTypePrefixConfig : IEntityTypeConfiguration<AccountTypePrefix>
+    {
+        public void Configure(EntityTypeBuilder<AccountTypePrefix> b)
+        {
+            b.ToTable("account_type_prefix");
+
+            b.HasKey(x => x.Prefix);
+
+            b.Property(x => x.Prefix)
+                .IsRequired()
+                .HasMaxLength(10);
+
+            b.Property(x => x.AccountType)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
---
+++ GLPack/Migrations/20260331060117_AddAccountTypePrefix.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GLPack.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountTypePrefix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "account_type_prefix",
+                columns: table => new
+                {
+                    Prefix = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    AccountType = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_account_type_prefix", x => x.Prefix);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "account_type_prefix");
+        }
+    }
+}
